4e17ee2329bff6fe40763eebabb3443480cf1912
pijush bera <pijushbera98@gmail.com>
2023-06-23 17:50:44 +0530

    1st

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        min:2,
+        max:100
+    },
+    {
+        ema
+    }
+})
